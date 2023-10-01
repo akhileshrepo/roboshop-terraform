97d25f55fef467e820960da5ba8dfb2110162a37
AKhilesh <kothapallyakhil123@gmail.com>
2023-10-02 01:16:11 +0530

    added vpc module

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
-module "components" {
+module "vpc" {
     for_each           = var.vpc
     source             = "git::https://github.com/akhileshrepo/tf-module-vpc.git"
     cidr               = each.value["cidr"]
     subnets            = each.value["subnets"]
+}
+
+output "vpc" {
+    value = module.vpc
 }
