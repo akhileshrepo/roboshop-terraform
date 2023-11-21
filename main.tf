4eab4afe0d544ed7e7c14edf6a207b8e6952f5f2
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-21 19:52:12 +0530

    updated code

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "vpc" {
     source = "git::https://github.com/akhileshrepo/tf-module-vpc.git"
     cidr = each.value["cidr"]
     subnets = each.value["subnets"]
-    default_vpc_id = var.vpc_default_vpc_id
+    vpc_default_vpc_id = var.vpc_default_vpc_id
     default_vpc_cidr = var.default_vpc_cidr
     default_vpc_route_table_id = var.default_vpc_route_table_id
 }
