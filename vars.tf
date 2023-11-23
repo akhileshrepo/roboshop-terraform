861dc7b7ee3c5b827bc3b52f9ffb8cdafae5701d
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-23 15:54:05 +0530

    updated code

--- vars.tf
+++ vars.tf
@@ -8,7 +8,6 @@ variable "alb" {}
 variable "docdb" {}
 variable "rds" {}
 variable "elasticache" {}
-variable "rabbitmq" {}
 variable "zone_id" {}
+variable "rabbitmq" {}
 variable "ssh_ingress_cidr" {}
-
