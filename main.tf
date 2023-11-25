aceb0dbc25563fc009c98077d1d632f886705f33
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-25 16:23:47 +0530

    updated code

--- main.tf
+++ main.tf
@@ -101,6 +101,7 @@ module "rabbitmq" {
 
 
 module "app" {
+    depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
     source = "git::https://github.com/akhileshrepo/tf-module-app.git"
     tags   =  var.tags
     env    =  var.env
