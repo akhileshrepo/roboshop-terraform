f71ec70cdc36dbba5389c76c7b2a1bed7bca670c
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-09 12:33:42 +0530

    updated code

--- vars.tf
+++ vars.tf
@@ -1,23 +1,5 @@
 variable "vpc" {}
 
 
-/*
-variable "default_vpc_id" {}
-variable "default_vpc_cidr" {}
-variable "default_vpc_route_table_id" {}
-variable "tags" {}
-variable "env" {}
-variable "alb" {}
-variable "docdb" {}
-variable "rds" {}
-variable "elasticache" {}
-variable "rabbitmq" {}
-variable "zone_id" {}
-variable "apps" {}
-variable "ssh_ingress_cidr" {}
-variable "kms_key_id" {}
-variable "acm_certificate_arn" {}
-variable "monitoring_ingress_cidr" {}
-*/
 
 
