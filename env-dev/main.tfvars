639926dc9f2af4a174bae508a3456d2fb4f6af8a
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-12 16:54:41 +0530

    updated code

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -55,6 +55,8 @@ docdb = {
     preferred_backup_window = "07:00-09:00"
     skip_final_snapshot     = true
     engine_version          = "4.0.0"
-    #engine_family           = "docdb4.0"
+    engine_family           = "docdb4.0"
+    instance_count          = 1
+    instance_class          = "db.t3.medium"
   }
 }
--- main.tf
+++ main.tf
@@ -42,4 +42,7 @@ module "docdb" {
   vpc_id                  = local.vpc_id
   sg_ingress_cidr         = local.app_subnets_cidr
   engine_version          = each.value["engine_version"]
+  engine_family          = each.value["engine_family"]
+  instance_count          = each.value["instance_count"]
+  instance_class         = each.value["instance_class"]
 }
