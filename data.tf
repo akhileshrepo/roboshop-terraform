faaa97db2991163bece3f0094d8b14809c2bd205
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-12 16:10:29 +0530

    updated code

--- data.tf
+++ data.tf
@@ -1,13 +1,13 @@
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
-}
-
 data "aws_subnets" "subnets" {
   filter {
     name   = "vpc-id"
     values = [var.default_vpc_id]
   }
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
 }
 
