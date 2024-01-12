1246254c4b9ed862ba92ff2352e52fe7066657d0
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-12 13:54:59 +0530

    updated code

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -51,6 +51,8 @@ alb = {
 
 docdb = {
   main = {
-
+    backup_retention_period = 5
+    preferred_backup_window = "07:00-09:00"
+    skip_final_snapshot     = true
   }
 }
--- main.tf
+++ main.tf
@@ -30,8 +30,13 @@ module "alb" {
 module "docdb" {
   source = "git::https://github.com/akhileshrepo/tf-module-alb.git"
 
-  for_each = var.docdb
-  tags = var.tags
-  env = var.env
-  subnet_ids = local.db_subnets
+  for_each                = var.docdb
+  tags                    = var.tags
+  env                     = var.env
+  subnet_ids              = local.db_subnets
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  vpc_id                  = local.vpc_id
+  sg_ingress_cidr         = local.app_subnets_cidr
 }
