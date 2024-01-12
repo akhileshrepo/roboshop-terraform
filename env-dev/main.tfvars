c10f80d2ab65cdfff97c8cb36bf1f99a7ca80684
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-13 00:23:07 +0530

    updated code

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -78,5 +78,7 @@ rds = {
     backup_retention_period  = 5
     preferred_backup_window  = "07:00-09:00"
     skip_final_snapshot     = true
+    instance_count         = 1
+    instance_class          = "db.t3.small"
   }
 }
--- main.tf
+++ main.tf
@@ -65,6 +65,8 @@ module "rds" {
   backup_retention_period  = each.value["backup_retention_period"]
   preferred_backup_window  = each.value["preferred_backup_window"]
   skip_final_snapshot    = each.value["skip_final_snapshot"]
+  instance_count       = each.value["instance_count"]
+  instance_class       = each.value["instance_class"]
 }
 
 
