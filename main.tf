5adacfa5ae653ce657a30b2a3bd0c819bed28793
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-17 16:48:21 +0530

    updated code

--- main.tf
+++ main.tf
@@ -134,6 +134,7 @@ module "apps" {
   subnet_ids       = local.app_subnets
 
   alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
+  listener = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
 }
 
 
