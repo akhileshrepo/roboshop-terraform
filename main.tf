e34b7f41f14e8834a52d9919b0d6510da865f225
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-17 15:58:35 +0530

    updated code

--- main.tf
+++ main.tf
@@ -133,7 +133,7 @@ module "apps" {
   vpc_id = local.vpc_id
   subnet_ids       = local.app_subnets
 
-  alb_name = lookup(lookup(module.alb, "private", null), "dns_name", null)
+  alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
 }
 
 
