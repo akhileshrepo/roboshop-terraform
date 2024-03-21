c144c5f49e618132bf58e7828a5a92c907356596
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-21 23:07:35 +0530

    updated code

--- locals.tf
+++ locals.tf
@@ -1,5 +1,8 @@
+
+
+/*
 locals {
   vpc_id      = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   app_subnets = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.id]
 }
-
+*/
