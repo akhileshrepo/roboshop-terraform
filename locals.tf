24f5fe9b40da3ac166a27730b53b3f705af4f4c3
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-22 12:46:29 +0530

    updated code

--- locals.tf
+++ locals.tf
@@ -6,6 +6,6 @@ locals {
   app_subnets = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.id]
   db_subnets = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "db", null), "subnet_ids", null) : v.id]
 
-  app_subnets_cidr = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "", null) : v.cidr_block]
+  app_subnets_cidr = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.cidr_block]
 }
 
