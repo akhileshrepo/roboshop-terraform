399ddb18ce5e5d0dbb8df8778b6bc7f55927d9bc
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-23 18:07:53 +0530

    updated code

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "vpc" {
   env                        = var.env
 }
 
+/*
 
 module "alb" {
   source = "git::https://github.com/akhileshrepo/tf-module-alb.git"
@@ -26,7 +27,7 @@ module "alb" {
   subnets = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
   sg_port = each.value["sg_port"]
 }
-
+*/
 
 module "docdb" {
   source = "git::https://github.com/akhileshrepo/tf-module-docdb.git"
