004ad0640e596b0abe19ffd9bbd6d1703f3e0b51
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-25 14:30:24 +0530

    updated code

--- main.tf
+++ main.tf
@@ -135,8 +135,10 @@ module "app" {
   min_size                  = each.value["min_size"]
   lb_priority               = each.value["lb_priority"]
 
-  alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
-  listener = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
+  private_alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
+  public_alb_name  = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
+  private_listener = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
+  public_listener  = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
 }
 
 
