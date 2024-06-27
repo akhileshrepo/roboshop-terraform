5c27c86028db9b8ce982849dbcf27218f9ad8b64
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-27 18:20:04 +0530

    updated code

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-# module "vpc" {
-#   source = "git::https://github.com/akhileshrepo/tf-module-vpc.git"
-#
-#   for_each                   = var.vpc
-#   cidr                       = each.value["cidr"]
-# }
+module "vpc" {
+  source = "git::https://github.com/akhileshrepo/tf-module-vpc.git"
+
+  for_each                   = var.vpc
+  cidr                       = each.value["cidr"]
+}
