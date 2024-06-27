193e72adf2019417bb7c70039ed2321c46b4ccc5
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-27 12:19:21 +0530

    updated code

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 module "vpc" {
   source = "git::https://github.com/akhileshrepo/tf-module-vpc.git"
 
-  for_each = var.vpc
-  cidr = each.value["cidr"]
-  subnets = each.value["subnets"]
-  default_vpc_id = var.default_vpc_id
-  default_vpc_cidr  = var.default_vpc_cidr
-  default_route_table_id = var.default_route_table_id
-  tags = var.tags
-  env = var.env
+  for_each                   = var.vpc
+  cidr                       = each.value["cidr"]
+  subnets                    = each.value["subnets"]
+  default_vpc_id             = var.default_vpc_id
+  default_vpc_cidr           = var.default_vpc_cidr
+  default_vpc_route_table_id = var.default_vpc_route_table_id
+  tags                       = var.tags
+  env                        = var.env
 }
 
 module "alb" {
--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "vpc" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
-variable "default_route_table_id" {}
+variable "default_vpc_route_table_id" {}
 variable "tags" {}
 variable "env" {}
 variable "alb" {}
