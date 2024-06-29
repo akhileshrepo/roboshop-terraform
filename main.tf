21d2fbec8183919e5d3b70835e7f55fde039ec2b
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-29 14:26:49 +0530

    updated code

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ module "vpc" {
   for_each = var.vpc
   cidr = each.value["cidr"]
   subnets = each.value["subnets"]
+  default_vpc_id = var.default_vpc_id
+  default_vpc_cidr = var.default_vpc_cidr
+  default_vpc_route_table_id = var.default_vpc_route_table_id
 }
 
 output "vpc" {
