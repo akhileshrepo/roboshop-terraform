6b1425f5cd979b7a44d2ff981d87f78590ec4f27
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-30 20:07:23 +0530

    updated code

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "vpc" {
 # }
 
 module "docdb" {
-  source = "git::https://github.com/raghudevopsb74/tf-module-docdb.git"
+  source = "git::https://github.com/akhileshrepo/tf-module-docdb.git"
   tags   = var.tags
   env    = var.env
 
