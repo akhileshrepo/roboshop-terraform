cadd3fa244b25e79620d2de4633da22931eac306
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-30 19:24:49 +0530

    updated code

--- main.tf
+++ main.tf
@@ -35,4 +35,8 @@ module "docdb" {
   backup_retention_period   = var.backup_retention_period
   preferred_backup_window   = var.preferred_backup_window
   skip_final_snapshot       = var.skip_final_snapshot
+  vpc_id                    = local.vpc_id
+  sg_ingress_cidr           = local.app_subnets_cidr
+  engine_version            = each.value["engine_version"]
 }
+
