a9ce37ba25d4b44db4a963b441b3828434a24119
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-03 11:09:34 +0530

    updated code

--- main.tf
+++ main.tf
@@ -117,5 +117,7 @@ module "app" {
   vpc_id                          = local.vpc_id
   sg_ingress_cidr                 = local.app_subnets_cidr
   subnet_ids                      = local.app_subnets
+
+  alb_name                        = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
 }
 
