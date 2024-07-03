78cac63f07e6c7df18d1756a9be5d8c059f0d7c0
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-03 19:56:40 +0530

    updated code

--- main.tf
+++ main.tf
@@ -12,17 +12,17 @@ module "vpc" {
 }
 
 module "alb" {
-  source = "git::https://github.com/akhileshrepo/tf-module-alb.git"
+ source = "git::https://github.com/akhileshrepo/tf-module-alb.git"
 
-  for_each                   = var.alb
-  lb_type                    = each.value["lb_type"]
-  internal                   = each.value["internal"]
-  sg_ingress_cidr            = each.value["sg_ingress_cidr"]
-  vpc_id                     = each.value["internal"] ? local.vpc_id : var.default_vpc_id
-  subnets                    = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
-  tags                       = var.tags
-  env                        = var.env
-  sg_port                    = each.value["sg_port"]
+ for_each            = var.alb
+ internal            = each.value["internal"]
+ lb_type             = each.value["lb_type"]
+ sg_ingress_cidr     = each.value["sg_ingress_cidr"]
+ vpc_id              = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+ subnets             = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
+ tags                = var.tags
+ env                 = var.env
+ sg_port             = each.value["sg_port"]
 }
 
 # module "docdb" {
@@ -99,31 +99,32 @@ module "alb" {
 # }
 
 module "app" {
-  source                          = "git::https://github.com/akhileshrepo/tf-module-app.git"
+#  depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
+ source     = "git::https://github.com/akhileshrepo/tf-module-app.git"
 
-  tags                            = var.tags
-  env                             = var.env
-  zone_id                         = var.zone_id
-  ssh_ingress_cidr                = var.ssh_ingress_cidr
-  default_vpc_id                  = var.default_vpc_id
-  az                              = var.az
+ tags                    = merge(var.tags, each.value["tags"])
+ env                     = var.env
+ zone_id                 = var.zone_id
+ ssh_ingress_cidr        = var.ssh_ingress_cidr
+ default_vpc_id          = var.default_vpc_id
+ az                      = var.az
 
-  for_each                        = var.apps
-  component                       = each.key
-  port                            = each.value["port"]
-  instance_type                   = each.value["instance_type"]
-  desired_capacity                = each.value["desired_capacity"]
-  max_size                        = each.value["max_size"]
-  min_size                        = each.value["min_size"]
-  lb_priority                     = each.value["lb_priority"]
+ for_each         = var.apps
+ component        = each.key
+ port             = each.value["port"]
+ instance_type    = each.value["instance_type"]
+ desired_capacity = each.value["desired_capacity"]
+ max_size         = each.value["max_size"]
+ min_size         = each.value["min_size"]
+ lb_priority      = each.value["lb_priority"]
 
-  vpc_id                          = local.vpc_id
-  sg_ingress_cidr                 = local.app_subnets_cidr
-  subnet_ids                      = local.app_subnets
+ sg_ingress_cidr = local.app_subnets_cidr
+ vpc_id          = local.vpc_id
+ subnet_ids      = local.app_subnets
 
-  private_alb_name                        = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
-  public_alb_name                        = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
-  private_listener                        = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
-  public_listener                        = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
+ private_alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
+ public_alb_name  = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
+ private_listener = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
+ public_listener  = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
 }
 
