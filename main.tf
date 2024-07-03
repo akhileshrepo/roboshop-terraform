1160cc9394d429c1028039dfe6e1fd5d8a5077e6
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-03 14:10:28 +0530

    updated code

--- main.tf
+++ main.tf
@@ -119,7 +119,9 @@ module "app" {
   sg_ingress_cidr                 = local.app_subnets_cidr
   subnet_ids                      = local.app_subnets
 
-  private_alb_name                        = lookup(lookup(lookup(module.alb, each.value["lb_type"], null), "alb", null), "dns_name", null)
-  priavte_listener                        = lookup(lookup(lookup(module.alb, each.value["lb_type"], null), "listener", null), "arn", null)
+  private_alb_name                        = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
+  public_alb_name                        = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
+  private_listener                        = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
+  public_listener                        = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
 }
 
