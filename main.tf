592c2d7d32818d60d67f8aef54ee4b6c867724c2
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-04 10:19:01 +0530

    updated code

--- main.tf
+++ main.tf
@@ -11,37 +11,37 @@ module "vpc" {
   env                        = var.env
 }
 
-module "alb" {
- source = "git::https://github.com/akhileshrepo/tf-module-alb.git"
-
- for_each            = var.alb
- internal            = each.value["internal"]
- lb_type             = each.value["lb_type"]
- sg_ingress_cidr     = each.value["sg_ingress_cidr"]
- vpc_id              = each.value["internal"] ? local.vpc_id : var.default_vpc_id
- subnets             = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
- tags                = var.tags
- env                 = var.env
- sg_port             = each.value["sg_port"]
-}
-
-# module "docdb" {
-#   source = "git::https://github.com/akhileshrepo/tf-module-docdb.git"
-#   tags   = var.tags
-#   env    = var.env
+# module "alb" {
+#  source = "git::https://github.com/akhileshrepo/tf-module-alb.git"
 #
-#   for_each                = var.docdb
-#   subnet_ids              = local.db_subnets
-#   backup_retention_period = each.value["backup_retention_period"]
-#   preferred_backup_window = each.value["preferred_backup_window"]
-#   skip_final_snapshot     = each.value["skip_final_snapshot"]
-#   vpc_id                  = local.vpc_id
-#   sg_ingress_cidr         = local.app_subnets_cidr
-#   engine_version          = each.value["engine_version"]
-#   engine_family           = each.value["engine_family"]
-#   instance_count          = each.value["instance_count"]
-#   instance_class          = each.value["instance_class"]
+#  for_each            = var.alb
+#  internal            = each.value["internal"]
+#  lb_type             = each.value["lb_type"]
+#  sg_ingress_cidr     = each.value["sg_ingress_cidr"]
+#  vpc_id              = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+#  subnets             = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
+#  tags                = var.tags
+#  env                 = var.env
+#  sg_port             = each.value["sg_port"]
 # }
+
+module "docdb" {
+  source = "git::https://github.com/akhileshrepo/tf-module-docdb.git"
+  tags   = var.tags
+  env    = var.env
+
+  for_each                = var.docdb
+  subnet_ids              = local.db_subnets
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  vpc_id                  = local.vpc_id
+  sg_ingress_cidr         = local.app_subnets_cidr
+  engine_version          = each.value["engine_version"]
+  engine_family           = each.value["engine_family"]
+  instance_count          = each.value["instance_count"]
+  instance_class          = each.value["instance_class"]
+}
 
 
 # module "rds" {
@@ -98,33 +98,33 @@ module "alb" {
 #   zone_id                         = var.zone_id
 # }
 
-module "app" {
-#  depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
- source     = "git::https://github.com/akhileshrepo/tf-module-app.git"
-
- tags                    = var.tags
- env                     = var.env
- zone_id                 = var.zone_id
- ssh_ingress_cidr        = var.ssh_ingress_cidr
- default_vpc_id          = var.default_vpc_id
- az                      = var.az
-
- for_each         = var.apps
- component        = each.key
- port             = each.value["port"]
- instance_type    = each.value["instance_type"]
- desired_capacity = each.value["desired_capacity"]
- max_size         = each.value["max_size"]
- min_size         = each.value["min_size"]
- lb_priority      = each.value["lb_priority"]
-
- sg_ingress_cidr = local.app_subnets_cidr
- vpc_id          = local.vpc_id
- subnet_ids      = local.app_subnets
-
- private_alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
- public_alb_name  = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
- private_listener = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
- public_listener  = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
-}
+# module "app" {
+# #  depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
+#  source     = "git::https://github.com/akhileshrepo/tf-module-app.git"
+#
+#  tags                    = var.tags
+#  env                     = var.env
+#  zone_id                 = var.zone_id
+#  ssh_ingress_cidr        = var.ssh_ingress_cidr
+#  default_vpc_id          = var.default_vpc_id
+#  az                      = var.az
+#
+#  for_each         = var.apps
+#  component        = each.key
+#  port             = each.value["port"]
+#  instance_type    = each.value["instance_type"]
+#  desired_capacity = each.value["desired_capacity"]
+#  max_size         = each.value["max_size"]
+#  min_size         = each.value["min_size"]
+#  lb_priority      = each.value["lb_priority"]
+#
+#  sg_ingress_cidr = local.app_subnets_cidr
+#  vpc_id          = local.vpc_id
+#  subnet_ids      = local.app_subnets
+#
+#  private_alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
+#  public_alb_name  = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
+#  private_listener = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
+#  public_listener  = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
+# }
 
