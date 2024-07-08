8a21dc8f9d819770890f0aca0ab37ca969a7e243
AKhilesh <kothapallyakhil123@gmail.com>
2024-07-08 12:12:50 +0530

    updated code

--- main.tf
+++ main.tf
@@ -117,7 +117,7 @@ module "app" {
  max_size         = each.value["max_size"]
  min_size         = each.value["min_size"]
  lb_priority      = each.value["lb_priority"]
-  parameters        =  each.value["parameters"]
+  parameters      =  each.value["parameters"]
 
  sg_ingress_cidr = local.app_subnets_cidr
  vpc_id          = local.vpc_id
